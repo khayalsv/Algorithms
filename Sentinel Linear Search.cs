8fbf625c650ed757f90b7ff7e26f9fe4a9c1e2a7
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-07-11 17:56:22 +0400

    update

---
+++ Sentinel Linear Search.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 180, 30, 60, 50 };
+            int n = arr.Length;
+            int key = 180;
+
+            sentinalSearch(arr, n, key);
+        }
+
+        static void sentinalSearch(int[] arr,int n,int key)  //array,array length,key
+        {
+            n = arr.Length;
+
+            int i = 0;
+            while (arr[i] != key)
+                i++;
+
+            if((i<n-1) || (arr[n-1]==key))
+                Console.WriteLine(key+ " is present" + " ar index "+ i);
+            else
+                Console.WriteLine("Element Not found");
+        }
+    }
+}
